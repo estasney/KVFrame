c1fec9c63daff40ae4457ef69ce8c2eb6774dc6d
estasney <16440579+estasney@users.noreply.github.com>
2018-12-14 11:23:28 -0500

    update layout make logo a relative reference

--- kvframe/main.py
+++ kvframe/main.py
@@ -1,10 +1,14 @@
 from kivy.app import App
 from kivy.config import Config
-Config.set('graphics', 'width', '1186')
-Config.set('graphics', 'height', '567')
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '600')
+import os
 
 class FrameApp(App):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.icon_path = os.path.realpath("resources/images/logo.png")
+
 
 if __name__ == "__main__":
     FrameApp().run()
