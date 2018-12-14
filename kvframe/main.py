ce8238e4cf5cf8b9e70a729b053c0ff5a71143b7
estasney <16440579+estasney@users.noreply.github.com>
2018-12-14 11:31:52 -0500

    update layout make logo a relative reference

--- kvframe/main.py
+++ kvframe/main.py
@@ -1,13 +1,18 @@
 from kivy.app import App
 from kivy.config import Config
+import os
+from kivy.uix.boxlayout import BoxLayout
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '600')
-import os
+
+
+class SplashScreen(BoxLayout):
+
+    icon_path = os.path.realpath("resources/images/logo.png")
+
 
 class FrameApp(App):
-    def __init__(self):
-        super().__init__()
-        self.icon_path = os.path.realpath("resources/images/logo.png")
+    pass
 
 
 if __name__ == "__main__":
