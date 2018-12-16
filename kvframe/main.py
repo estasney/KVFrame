05aa7279c5da2405d52e95dc9dd1185db978e58b
eric <estasney@users.noreply.github.com>
2018-12-15 22:02:33 -0500

    screen management

--- kvframe/main.py
+++ kvframe/main.py
@@ -2,17 +2,26 @@ from kivy.app import App
 from kivy.config import Config
 import os
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '600')
 
 
-class SplashScreen(BoxLayout):
-
-    icon_path = os.path.realpath("resources/images/logo.png")
-
-class FrameApp(App):
+class MainScreen(Screen):
     pass
 
+class SplashScreen(Screen):
+    icon_path = os.path.realpath("resources/images/logo.png")
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return presentation
 
 if __name__ == "__main__":
-    FrameApp().run()
+    MainApp().run()
