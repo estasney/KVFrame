66a35d10f38411974b4d51886c676501f64e2730
eric <estasney@users.noreply.github.com>
2018-12-20 21:07:55 -0500

    add another accordionitem

--- kvframe/main.py
+++ kvframe/main.py
@@ -41,13 +41,16 @@ class StatusComponent(BoxLayout):
     pass
 
 class RoundedButton(Button):
-    UNPRESSED_COLOR = get_color_from_hex("#ff000d")
-    PRESSED_COLOR = get_color_from_hex("#be0119")
     DISABLED_COLOR = get_color_from_hex("#9e9ea2")
     FONT_COLOR = get_color_from_hex("#ffffff")
 
     def collide_point(self, x, y):
         return Vector(x, y).distance(self.center) <= self.width / 2
+
+class RedButton(RoundedButton):
+    UNPRESSED_COLOR = get_color_from_hex("#ff000d")
+    PRESSED_COLOR = get_color_from_hex("#be0119")
+
 
 class GreenButton(RoundedButton):
     UNPRESSED_COLOR = get_color_from_hex("#21fc0d")
