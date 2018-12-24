8686a221818b3cc46a463446a7de01e9614bce4e
eric <estasney@users.noreply.github.com>
2018-12-24 15:18:52 -0500

    add additional texture and randomize what is shown

--- kvballgame/bounce.py
+++ kvballgame/bounce.py
@@ -5,12 +5,13 @@ if 'debian' in platform.platform():
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+    ObjectProperty, StringProperty, ListProperty
 from kivy.vector import Vector
 from kivy.core.image import Image
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
 from math import log
+import glob
 import random
 import os
 
@@ -24,12 +25,24 @@ class Ball(Widget):
     angle = NumericProperty(0)
 
     sound = SoundLoader.load(os.path.realpath("resources/audio/boing.wav"))
-    my_texture = Image(os.path.realpath("resources/images/basketball-texture.png"))
+
+    ball_texture = ObjectProperty()
+    ball_texture_path = StringProperty()
+
+    def reload_texture(self, *args, **kwargs):
+        image = Image(os.path.realpath(self.ball_texture_path))
+        self.ball_texture = image.texture
+        self.canvas.ask_update()
+
+    def on_ball_texture_path(self, *args, **kwargs):
+        self.reload_texture(self)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
         if abs(self.velocity_x) > 1 or abs(self.velocity_y) > 1:
             self.angle += log(max([abs(self.velocity_x), abs(self.velocity_y)]))
+
+
 
     def decay(self, *args, **kwargs):
 
@@ -56,8 +69,22 @@ class Ball(Widget):
 class BounceGame(Widget):
 
     ball = ObjectProperty(None)
+    ball_textures = glob.glob("resources/images/balls/*.png")
+
+    def random_chance(self, low, high):
+        r = random.randrange(low, high)
+        if r == low:
+            return True
+        else:
+            return False
+
+    def find_new_texture(self):
+        current_texture = self.ball.ball_texture_path
+        new_textures = [t for t in self.ball_textures if t != current_texture]
+        return random.choice(new_textures)
 
     def serve_ball(self, vel=(4, 0)):
+        self.ball.ball_texture_path = self.ball_textures[0]
         self.ball.center = self.center
         self.ball.velocity = vel
 
@@ -75,6 +102,9 @@ class BounceGame(Widget):
     def on_touch_down(self, touch):
         if self.ball.collide_point(touch.x, touch.y):
             self.ball.boing()
+            do_texture_change = self.random_chance(0, 2)
+            if do_texture_change:
+                self.ball.ball_texture_path = self.find_new_texture()
 
 
 class BounceApp(App):
