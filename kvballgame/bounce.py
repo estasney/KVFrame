6576743679954389b246a4b63e393dd759c35654
eric <estasney@users.noreply.github.com>
2018-12-25 21:51:31 -0500

    add gravity and vector collisions

--- kvballgame/bounce.py
+++ kvballgame/bounce.py
@@ -2,6 +2,7 @@ import platform
 import os
 if 'debian' in platform.platform():
     os.environ['KIVY_GL_BACKEND'] = 'gl'
+    os.environ['KIVY_AUDIO'] = 'sdl2'
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,\
@@ -21,6 +22,9 @@ class Ball(Widget):
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    x_decay = NumericProperty(0.99)
+    y_decay = NumericProperty(-0.95)
+
     spin_rate = NumericProperty(0)
     angle = NumericProperty(0)
 
@@ -37,27 +41,66 @@ class Ball(Widget):
     def on_ball_texture_path(self, *args, **kwargs):
         self.reload_texture(self)
 
+    def get_resultant_vector(self, touch):
+        col_vector = Vector(touch.pos) - Vector(self.center)
+        col_vector_mag = col_vector.length()
+        random_magnitude = random.randint(5, 10)
+
+        return random_magnitude / col_vector * col_vector_mag
+
+
     def move(self):
+
+        if self.y <= self.parent.y:
+
+            if self.y < self.parent.y:
+                self.pos = Vector(0, self.parent.y - self.y) + self.pos
+            if abs(self.velocity_y) < (8 * abs(self.y_decay)):
+                self.velocity_y = 0
+                self.y_decay = 0
+            else:
+                self.velocity_y *= -0.8
+
+        if self.top >= self.parent.top:
+
+            if self.top > self.parent.top:
+                self.pos = Vector(0, self.parent.top - self.top) + self.pos
+            self.velocity_y = abs(self.velocity_y) * 0.8 * -1
+
+        if self.x <= self.parent.x:
+
+            if self.x < self.parent.x:
+                self.pos = Vector(self.parent.x - self.x + 3, 0) + self.pos
+            self.velocity_x *= -0.8
+
+        if (self.x + self.width) >= self.parent.width:
+
+            if (self.x + self.width) > self.parent.width:
+                self.pos = Vector(self.parent.width - self.x - self.width - 3, 0) + self.pos
+            self.velocity_x *= -0.8
+
+
         self.pos = Vector(*self.velocity) + self.pos
+
         if abs(self.velocity_x) > 1 or abs(self.velocity_y) > 1:
             self.angle += log(max([abs(self.velocity_x), abs(self.velocity_y)]))
 
+        self.decay()
 
     def decay(self, *args, **kwargs):
 
-        self.velocity_x *= 0.9
-        self.velocity_y *= 0.9
+        self.velocity_x *= self.x_decay
+        self.velocity_y += self.y_decay
+        
 
-    def boing(self):
-        vx = random.randint(10, 25)
-        vy = random.randint(10, 25)
-        vz = random.randint(10, 25)
-        dx = random.choice([1, -1])
-        dy = random.choice([1, -1])
-        dz = random.choice([1, -1])
+    def boing(self, touch):
 
-        self.velocity_x += (vx * dx)
-        self.velocity_y += (vy * dy)
+        resultant_vector = self.get_resultant_vector(touch)
+        print(resultant_vector)
+
+        self.velocity_x += resultant_vector[0]
+        self.velocity_y += resultant_vector[1]
+        self.y_decay = -0.95
         self.sound.play()
 
     def on_angle(self, item, angle):
@@ -82,32 +125,28 @@ class BounceGame(Widget):
         new_textures = [t for t in self.ball_textures if t != current_texture]
         return random.choice(new_textures)
 
-    def serve_ball(self, vel=(4, 0)):
+    def serve_ball(self, vel=(4, 4)):
         self.ball.ball_texture_path = random.choice(self.ball_textures)
         self.ball.center = self.center
         self.ball.velocity = vel
 
     def update(self, dt):
+
         self.ball.move()
 
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= 1.1
-            self.ball.velocity_y *= -1
 
-        if (self.ball.x < self.x) or ((self.ball.x + self.ball.width) > self.width):
-            self.ball.velocity_x *= 1.1
-            self.ball.velocity_x *= -1
 
     def on_touch_down(self, touch):
         if self.ball.collide_point(touch.x, touch.y):
-            self.ball.boing()
+            self.ball.boing(touch)
             do_texture_change = self.random_chance(0, 2)
             if do_texture_change:
                 self.ball.ball_texture_path = self.find_new_texture()
 
     def on_touch_move(self, touch):
+
         if self.ball.collide_point(touch.x, touch.y):
-            self.ball.boing()
+            self.ball.boing(touch)
             do_texture_change = self.random_chance(0, 2)
             if do_texture_change:
                 self.ball.ball_texture_path = self.find_new_texture()
@@ -117,8 +156,8 @@ class BounceApp(App):
     def build(self):
         game = BounceGame()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        Clock.schedule_interval(game.ball.decay, 0.1)
+        Clock.schedule_interval(game.update, 1/60)
+
         return game
 
 
