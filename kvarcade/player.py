3487d62dea7511d4012065696f2e9dd5e8956e54
estasney <estasney@users.noreply.github.com>
2020-06-01 20:31:53 -0400

    walk folders

--- kvarcade/player.py
+++ kvarcade/player.py
@@ -7,13 +7,20 @@ from kivy.core.audio import SoundLoader
 class SoundCycler:
 
     def __init__(self, folder, alpha=1, beta=1):
-        self.files = [os.path.join(folder, f) for f in os.listdir(folder)]
+        self.files = self._find_files(folder)
         self.alpha = alpha
         self.beta = beta
         self.filecycler = cycle(self.files)
         self.current_file = next(self.filecycler)
         self.sound = SoundLoader.load(self.current_file)
         self.sound.loop = True
+
+    def _find_files(self, folder):
+        files = []
+        for root, _, files in os.walk(folder):
+            for file in files:
+                files.append(os.path.join(root, file))
+        return files
 
     def stop(self, *args, **kwargs):
         self.sound.stop()
