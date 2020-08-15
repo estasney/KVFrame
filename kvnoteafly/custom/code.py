24c1d84544f1cb18fb3a11392f3314556a715380
estasney <estasney@users.noreply.github.com>
2020-08-15 17:37:46 -0400

    refactor so code notes are using code objects, rst uses rst objects

--- kvnoteafly/custom/code.py
+++ kvnoteafly/custom/code.py
@@ -4,10 +4,10 @@ from kivy.uix.anchorlayout import AnchorLayout
 from utils import import_kv
 
 import_kv(__file__)
-from kivy.uix.rst import RstDocument
-class ContentRST(AnchorLayout):
+
+class ContentCode(AnchorLayout):
     note_text = StringProperty()
 
     def __init__(self, content_data, **kwargs):
         self.note_text = content_data['text']
-        super(ContentRST, self).__init__(**kwargs)
+        super(ContentCode, self).__init__(**kwargs)
