b2f92b82238ad3b7f41a130c7b578c20563f6209
estasney <estasney@users.noreply.github.com>
2020-08-31 11:27:19 -0400

    add atlas_utils for unpacking atlas

--- kvnoteafly/atlas_utils/unpack.py
+++ kvnoteafly/atlas_utils/unpack.py
@@ -23,7 +23,7 @@ def cropbox(coords, im):
 def unpack_atlas_imgs(img_fp: str, imgs: Dict[str, Tuple[int, int, int, int]], output_dir: str):
     im = Image.open(img_fp)
     _, img_ext = os.path.splitext(img_fp)
-    for img_name, img_coords in imgs:
+    for img_name, img_coords in imgs.items():
         cc = cropbox(img_coords, im)
         cropped_im = im.crop((cc.x, cc.y, cc.w, cc.h))
         cropped_im_fp = os.path.join(output_dir, f"{img_name}{img_ext}")
@@ -42,8 +42,10 @@ def unpack_atlas(atlas_fp):
 
     atlas_dir, _ = os.path.split(atlas_fp)
     for atlas_img, imgs in atlas_data.items():
-        atlas_fp = os.path.join(atlas_dir, atlas_fp)
-        unpack_atlas_imgs(atlas_fp, imgs, atlas_dir)
+        atlas_img_fp = os.path.join(atlas_dir, atlas_img)
+        print(atlas_img_fp)
+        print(imgs)
+        unpack_atlas_imgs(atlas_img_fp, imgs, atlas_dir)
 
 
 if __name__ == '__main__':
