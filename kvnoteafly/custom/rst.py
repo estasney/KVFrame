06bd4bd7dfd4a0fc93cc7ac5508a4e652b0c8d98
estasney <estasney@users.noreply.github.com>
2020-09-01 00:20:57 -0400

    with the dark background we can render readable literal blocks or readable tables but not both. Adds regex to detect if a table is included.

--- kvnoteafly/custom/rst.py
+++ kvnoteafly/custom/rst.py
@@ -1,13 +1,22 @@
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty, BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 
 from utils import import_kv
+import re
 
 import_kv(__file__)
-from kivy.uix.rst import RstDocument
+
+
+table_re = re.compile(r"((?:=|-)+ +(?:=|-)+)")
+
 class ContentRST(AnchorLayout):
     note_text = StringProperty()
+    is_table = BooleanProperty(False)
+    para_color = StringProperty('000000')
 
     def __init__(self, content_data, **kwargs):
         self.note_text = content_data['text']
+        if table_re.findall(self.note_text):
+            self.is_table = True
+            self.para_color = 'ffffff'
         super(ContentRST, self).__init__(**kwargs)
