a246d0c3f3aa81d62577864e4f2c378f07d193a5
estasney <estasney@users.noreply.github.com>
2020-09-07 23:35:58 -0400

    add x y dimensions

--- kvyouget/dl/utils.py
+++ kvyouget/dl/utils.py
@@ -3,12 +3,14 @@ from collections import namedtuple
 from contextlib import redirect_stdout
 from io import StringIO
 from typing import Dict
+import re
 
 from you_get.common import any_download
 
-ITag = namedtuple("ITag", "itag, container, quality, size")
+ITag = namedtuple("ITag", "itag, container, quality, size, x, y")
 Result = namedtuple("Result", "title, itags")
 
+xy_re = re.compile(r"([\d]{3,4})(?:x)([\d]{3,4})")
 
 def get_url_options(url: str) -> Result:
     s = StringIO()
@@ -28,4 +30,9 @@ def _parse_itag(itag: Dict) -> ITag:
     itag_quality = itag["quality"]
     container = itag["container"]
     size = itag.get("size", 0)
-    return ITag(itag=itag_id, container=container, quality=itag_quality, size=size)
+    xy_size = xy_re.search(itag_quality)
+    if not xy_size:
+        x, y, = 0, 0
+    else:
+        x, y = [int(i) for i in xy_size.groups()]
+    return ITag(itag=itag_id, container=container, quality=itag_quality, size=size, x=x, y=y)
