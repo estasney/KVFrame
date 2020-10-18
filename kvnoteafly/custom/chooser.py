be7f0b75bad324fa733c42ea420bd6ef9f8170f7
estasney <estasney@users.noreply.github.com>
2020-10-17 22:32:48 -0400

    note category chooser now scrolls

--- kvnoteafly/custom/chooser.py
+++ kvnoteafly/custom/chooser.py
@@ -1,31 +1,53 @@
+from kivy.core.window import Window
 from kivy.properties import ObjectProperty, ListProperty, StringProperty
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.stacklayout import StackLayout
 from kivy.vector import Vector
+
 from utils import import_kv
 
 import_kv(__file__)
 
 
+class NoteCategoryChooserScrollWrapper(ScrollView):
+    manager = ObjectProperty()
+    child_object = ObjectProperty()
+    categories = ListProperty()
+
+    def category_selected(self, instance):
+        self.manager.category_selected(instance)
+
+    def on_categories(self, instance, value):
+        if not self.children:
+            layout = NoteCategoryChooser(size_hint=(1, None), width=Window.width)
+            layout.bind(minimum_height=layout.setter('height'))
+            self.add_widget(layout)
+            self.child_object = layout
+            self.children[0].set(value)
+
+
 class NoteCategoryChooser(StackLayout):
     manager = ObjectProperty()
-    categories = ListProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def on_parent(self, *args, **kwargs):
+        self.manager = args[1]
+
     def category_callback(self, instance):
         self.manager.category_selected(instance)
 
-    def on_categories(self, instance, value):
-        category_widgets = [NoteCategoryButton(text=cat, size_hint=(0.25, 0.25)) for cat in value]
+    def set(self, value):
+        category_widgets = [NoteCategoryButton(text=cat, size_hint=(0.25, None)) for cat in value]
         for cat in category_widgets:
             cat.bind(on_release=self.category_callback)
             self.add_widget(cat)
 
-class NoteCategoryButton(ButtonBehavior, BoxLayout):
 
+class NoteCategoryButton(ButtonBehavior, BoxLayout):
     source = StringProperty()
     text = StringProperty()
 
@@ -36,5 +58,3 @@ class NoteCategoryButton(ButtonBehavior, BoxLayout):
 
     def collide_point(self, x, y):
         return Vector(x, y).distance(self.center) <= self.width / 2
-
-
